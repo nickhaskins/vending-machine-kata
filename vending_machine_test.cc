b1dd2a17f90dbb85e3b0b8c4b94524aab82e6b22
Nick <njhaskins@gmail.com>
2017-08-06 10:40:42 -0700

    Factor out common test setup

--- vending_machine_test.cc
+++ vending_machine_test.cc
@@ -5,41 +5,41 @@
 namespace vending_machine_kata {
 namespace {
 
-TEST(VendingMachineTest, DisplaysInsertCoinsWhenNoCoinsAdded) {
-	VendingMachine vending_machine;
-	EXPECT_EQ(vending_machine.GetDisplay(), "INSERT COIN");
+class VendingMachineTest : public ::testing::Test {
+ protected:
+  VendingMachine vending_machine_;
+};
+
+TEST_F(VendingMachineTest, DisplaysInsertCoinsWhenNoCoinsAdded) {
+	EXPECT_EQ(vending_machine_.GetDisplay(), "INSERT COIN");
 }
 
-TEST(VendingMachineTest, AddQuarterDisplaysCurrentAmount) {
-	VendingMachine vending_machine;
-	vending_machine.AddCoin(QUARTER);
-	EXPECT_EQ(vending_machine.GetDisplay(), "0.25");
+TEST_F(VendingMachineTest, AddQuarterDisplaysCurrentAmount) {
+	vending_machine_.AddCoin(QUARTER);
+	EXPECT_EQ(vending_machine_.GetDisplay(), "0.25");
 }
 
-TEST(VendingMachineTest, CurrentAmountDependsOnCoinAdded) {
-	VendingMachine vending_machine;
-	vending_machine.AddCoin(DIME);
-	EXPECT_EQ(vending_machine.GetDisplay(), "0.10");
+TEST_F(VendingMachineTest, CurrentAmountDependsOnCoinAdded) {
+	vending_machine_.AddCoin(DIME);
+	EXPECT_EQ(vending_machine_.GetDisplay(), "0.10");
 
-	vending_machine = VendingMachine();
-	vending_machine.AddCoin(NICKEL);
-	EXPECT_EQ(vending_machine.GetDisplay(), "0.05");
+	vending_machine_ = VendingMachine();
+	vending_machine_.AddCoin(NICKEL);
+	EXPECT_EQ(vending_machine_.GetDisplay(), "0.05");
 }
 
-TEST(VendingMachineTest, AddingMultipleCoinsSumsCurrentAmount) {
-	VendingMachine vending_machine;
-	vending_machine.AddCoin(DIME);
-	vending_machine.AddCoin(QUARTER);
-	EXPECT_EQ(vending_machine.GetDisplay(), "0.35");
+TEST_F(VendingMachineTest, AddingMultipleCoinsSumsCurrentAmount) {
+	vending_machine_.AddCoin(DIME);
+	vending_machine_.AddCoin(QUARTER);
+	EXPECT_EQ(vending_machine_.GetDisplay(), "0.35");
 }
 
-TEST(VendingMachineTest, AddingPennyRejected) {
-	VendingMachine vending_machine;
-	vending_machine.AddCoin(PENNY);
-	EXPECT_EQ(vending_machine.GetDisplay(), "INSERT COIN");
+TEST_F(VendingMachineTest, AddingPennyRejected) {
+	vending_machine_.AddCoin(PENNY);
+	EXPECT_EQ(vending_machine_.GetDisplay(), "INSERT COIN");
 	std::vector<Coin> expected_coin_return;
 	expected_coin_return.push_back(PENNY);
-	EXPECT_EQ(vending_machine.GetCoinReturn(), expected_coin_return);
+	EXPECT_EQ(vending_machine_.GetCoinReturn(), expected_coin_return);
 }
 
 }  // namespace
